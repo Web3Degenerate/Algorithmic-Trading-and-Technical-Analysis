01172efb26a1a3848522c6d6def11775dcbde874
Hostnomics <hostnomics@gmail.com>
2023-11-30 22:11:48 -0500

    Added cancelOrder() to index.js' newOrder() call.

--- bot-trading/index.js
+++ bot-trading/index.js
@@ -17,7 +17,29 @@ const restClient = new GeminiAPI({key, secret, sandbox:true});
 // Create an order (7:05): https://academy.moralis.io/lessons/using-gemini-buy-order
 // Make a limit order: 
 // gemini package will create the new order and send it.
-// THEN console log the response: 
-restClient.newOrder({amount:10, price:100, side:"buy", symbol: "btcusd" }).then(response => console.log(response));
+// THEN console log the response (Call from terminal with `node index.js`):
+restClient.newOrder({amount:10, price:100, side:"buy", symbol: "btcusd" })
+// .then(response => console.log(response))
+// .then(response => console.log(response.order_id))
+.then(response => restClient.cancelOrder({order_id: response.order_id}))
+.then(response => console.log(response))
+.catch(error => console.log("Error in newOrder() located in index.js was: ",error));
 
-// Call from terminal with `node index.js`
+
+
+
+
+// Work through the REST API documentation on gemini-api github: 
+
+// getAllSymbols().then(response => console.log(response));
+// restClient.getAllSymbols().then(response => console.log(response));  // returns updated symbol pairs available on gemini
+
+// get latest price of ticker
+// timestamp seconds after unix epoch
+// restClient.getTicker("btcusd").then(response => console.log(response));
+
+
+// restClient.getOrderBook("symbol_here", params={}) //often params optional
+
+
+
